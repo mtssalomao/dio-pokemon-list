d1fe04dd8d99d0a5a5f75ea97f5e56be91b3661e
Matheus Salomão <98296114+mtssalomao@users.noreply.github.com>
2023-08-30 20:19:31 -0300

    Add files via upload

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,6 +1,6 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 function convertPokemonToLi(pokemon) {
   return `
@@ -21,14 +21,14 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById(`pokemonList`)
 
-fetch(url)
-  .then((response) => response.json())
-  .then((jsonBody) => jsonBody.results)
-  .then((pokemons) => {
-        for(let i = 0; i < pokemons.length; i++) {
+pokeApi.getPokemons().then((pokemons) => {
+    const listItens = []   
+
+    
+    for(let i = 0; i < pokemons.length; i++) {
             const pokemon = pokemons[i];
-            console.log(convertPokemonToLi(pokemon))
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+            listItens.push(convertPokemonToLi(pokemon))
         }
+
+        console.log(listItens)
   })
-  .catch((error) => console.error(error))
